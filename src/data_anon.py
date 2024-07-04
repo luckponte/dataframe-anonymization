3edb2b2a24dbce5f87c4e3914df18ef2e74cd79a
Lucas Correia <lucas.correia@vwds.pt>
2024-07-04 15:58:49 +0100

    ✨ Create a function that anonymizes a data frame
    This is based on a list of columns that are to be anonymized. Currently anonymizes strings and numbers

--- src/data_anon.py
+++ src/data_anon.py
@@ -1,9 +1,14 @@
+import hashlib
 from pandas import DataFrame
 
 
-def anonymize_data(data_identifiable: DataFrame) -> DataFrame:
-    data_identifiable['name'] = 'Anonymous'
-    data_identifiable['email'] = 'anonymous@example.com'
+def anonymize(value):
+    if isinstance(value,str):
+        return hashlib.sha256(value.encode()).hexdigest()
+    return 0
 
-    data_identifiable['employee_id'] = 0
+def anonymize_data(data_identifiable: DataFrame, anonymize_columns: list) -> DataFrame:
+    for column in anonymize_columns:
+        data_identifiable[column] = data_identifiable[column].apply(anonymize)
+
     return data_identifiable
--- tests/test_data_anon.py
+++ tests/test_data_anon.py
@@ -1,35 +1,78 @@
+import hashlib
 import pandas as pd
+import pytest
 
 from src.data_anon import anonymize_data
 
-def test_data_anon():
-    data_identifiable = pd.DataFrame({
-    'name': ['John Doe'],
-    'email': ['john.doe@example.com'],
-    'age': [30]
-})
+def hash_string(original_list):
+    return [hashlib.sha256(value.encode()).hexdigest() for value in original_list]
 
-    data_expected_anonimized = pd.DataFrame({
-    'name': ['Anonymous'],
-    'email': ['anonymous@example.com'],
-    'age': [30]
-})
-    
-    data_actual_anonimized = anonymize_data(data_identifiable)
-    assert data_expected_anonimized.equals(data_actual_anonimized)
+test_cases = [
+    {
+        "given": {
+            "data": pd.DataFrame({
+                'name': ['John Doe'],
+                'email': ['john.doe@example.com'],
+                'age': [30]
+            }),
+            "anonymize": ['name','email'],
+        },
+        "expect":pd.DataFrame({
+            'name': hash_string(['John Doe']),
+            'email': hash_string(['john.doe@example.com']),
+            'age': [30]
+        }),
+    },
+    {
+        "given": {
+            "data": pd.DataFrame({
+                'name': ['Alice Smith','Bob Johnson'],
+                'email': ['alice.smith@company.com','bob.johnson@company.com'],
+                'age': [28,35],
+                'employee_id': [12345,67890]
+            }),
+            "anonymize": ['name','email','employee_id']
+        },
+        "expect":pd.DataFrame({
+            'name': hash_string(['Alice Smith','Bob Johnson']),
+            'email': hash_string(['alice.smith@company.com','bob.johnson@company.com']),
+            'age': [28,35],
+            'employee_id': [0,0]
+        }),
+    },
+    {
+        "given": {
+            "data": pd.DataFrame({
+                'name': ['Alice Smith','Bob Johnson'],
+                'email': ['alice.smith@company.com','bob.johnson@company.com'],
+                'age': [28,35],
+                'employee_id': [12345,67890]
+            }),
+            "anonymize": ['name','email']
+        },
+        "expect":pd.DataFrame({
+            'name': hash_string(['Alice Smith','Bob Johnson']),
+            'email': hash_string(['alice.smith@company.com','bob.johnson@company.com']),
+            'age': [28,35],
+            'employee_id': [12345,67890]
+        }),
+    },
+    {
+        "given": {
+            "data": pd.DataFrame({
+                'client_name': ['John Doe', 'Jane Smith', 'John Doe', 'Alice Johnson', 'Bob Brown'],
+                'quotation': [100.50, 200.75, 150.25, 300.00, 250.40]
+            }),
+            "anonymize": ['client_name'],
+        },
+        "expect": pd.DataFrame({
+            'client_name': hash_string(['John Doe', 'Jane Smith', 'John Doe', 'Alice Johnson', 'Bob Brown']),
+            'quotation': [100.50, 200.75, 150.25, 300.00, 250.40]
+        }),
+    }
+]
 
-    data_identifiable = pd.DataFrame({
-    'name': ['Alice Smith','Bob Johnson'],
-    'email': ['alice.smith@company.com','bob.johnson@company.com'],
-    'age': [28,35],
-    'employee_id': [12345,67890]
-})
-    
-    data_expected_anonimized = pd.DataFrame({
-    'name': ['Anonymous','Anonymous'],
-    'email': ['anonymous@example.com','anonymous@example.com'],
-    'age': [28,35],
-    'employee_id': [0,0]
-})
-    data_actual_anonimized = anonymize_data(data_identifiable)
-    assert data_expected_anonimized.equals(data_actual_anonimized)
+@pytest.mark.parametrize("case", test_cases)
+def test_data_anon(case):
+    data_actual_anonimized = anonymize_data(case['given']['data'],case['given']['anonymize'])
+    assert case['expect'].equals(data_actual_anonimized), 'The anonymized data does not match the expected values'
